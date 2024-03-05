c5c3a49d8ef4174e8b8058ccb23232a4496a0d01
coumba <coumbissthiam96@gmail.com>
2024-03-05 13:17:46 +0000

    update docker configuration

--- src/main/java/com/exem/springapp/SpringappApplication.java
+++ src/main/java/com/exem/springapp/SpringappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringappApplication {
+public class  SpringappApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringappApplication.class, args);
